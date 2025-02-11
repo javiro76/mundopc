7bd8ff750f1d44646e67b3995548a3140f8ac836
javier <javier.rodriguez@dev-codes.io>
2025-02-10 21:33:51 -0500

    ADD: class monitor

--- src/mundopc/Main.java
+++ src/mundopc/Main.java
@@ -1,7 +1,21 @@
 package mundopc;
 
+import mundopc.modelo.Monitor;
+import mundopc.modelo.Raton;
+import mundopc.modelo.Teclado;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+       //creado objetos
+        Raton ratonLenovo = new Raton("Bluetooth", "Lenovo");
+        System.out.println(ratonLenovo);
+
+        Teclado tecladoLenovo = new Teclado("Bluetooth", "Lenovo");
+        System.out.println(tecladoLenovo);
+
+        Monitor monitorLenovo = new Monitor("Lenovo", 32);
+        System.out.println(monitorLenovo);
+
     }
 }
